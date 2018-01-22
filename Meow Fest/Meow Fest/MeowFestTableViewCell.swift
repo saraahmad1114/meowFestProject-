39eae3ccae19d05a6c6a012ff2c2d7de08cdb132
saraahmad1114 <asara1114@gmail.com>
2018-01-22 16:41:45 -0500

    added file

---
+++ Meow Fest/Meow Fest/MeowFestTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MeowFestTableViewCell.swift
+//  Meow Fest
+//
+//  Created by Sara Ahmad on 1/22/18.
+//  Copyright © 2018 Sara Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class MeowFestTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
